82f893822deb2868f67acd34bfe70b02e3d561dc
DavidPopovich3261 <8534397@gmail.com>
2025-10-23 12:54:07 +0300

    Project ready

--- game_logic/game.py
+++ game_logic/game.py
@@ -13,7 +13,7 @@ def create_player(name:str='Ai') -> dict:
 def init_game()->dict:
 
     p2=create_player()
-    p1=create_player(input('Please enter your name.'))
+    p1=create_player(input('Please enter your name'))
     deck1=deck.create_deck()
     deck1=deck.shuffle(deck1)
     for i in range(52):
@@ -33,13 +33,15 @@ def  play_round(p1:dict,p2:dict):
     p1_rotation = p1["hand"].pop(-1)
     p2_rotation = p2["hand"].pop(-1)
     if p1_rotation['value'] > p2_rotation['value']:
-        p1['won_pile'].append(p1_rotation,p2_rotation)
-        print(p1['name'],'winner')
+        p1['won_pile'].append(p1_rotation)
+        p1['won_pile'].append(p2_rotation)
+        print(p1['name'],'winner',p1_rotation)
     elif p2_rotation['value'] > p1_rotation['value']:
-        p2['won_pile'].append(p1_rotation,p2_rotation)
-        print(p2['name'],'winner')
+        p2['won_pile'].append(p1_rotation)
+        p2['won_pile'].append(p2_rotation)
+        print(p2['name'],'winner',p2_rotation)
     else:
-         print('draw')
+         print('draw',p1_rotation)
 
 
 
--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 import random
 from game_logic import game
 
-a=game.init_game()
-print(a['player_1']['hand'])
-
-
-
-b=game.play_round(a['player_1'],a['player_2'])
-
-
-
-
 
 
 
@@ -19,4 +9,18 @@ b=game.play_round(a['player_1'],a['player_2'])
 
 
 if __name__ == "__main__":
-    pass
+    game_now=game.init_game()
+    while len(game_now['player_1']['hand']) and len(game_now['player_2']['hand']):
+        game.play_round(game_now['player_1'], game_now['player_2'])
+    print(game_now['player_1']['name'],len(game_now['player_1']['won_pile']))
+    print(game_now['player_2']['name'],len(game_now['player_2']['won_pile']))
+    if len(game_now['player_2']['won_pile']) > len(game_now['player_1']['won_pile']):
+        print(game_now['player_2']['name'],'winner')
+    elif len(game_now['player_1']['won_pile']) > len(game_now['player_2']['won_pile']):
+        print(game_now['player_1']['name'],'winner')
+    else:
+        print('draw')
+
+
+
+
