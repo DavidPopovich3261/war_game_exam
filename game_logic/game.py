6b4a00d16c7edba8f26e08fb8d006144e7a14305
DavidPopovich3261 <8534397@gmail.com>
2025-10-23 11:53:49 +0300

    init_game1

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,14 +1,30 @@
+from utils import deck
 
 
 
 
-def create_player(name:str) -> dict:
-    pass
+
+def create_player(name:str='Ai') -> dict:
+    player={'name':name,'hand':[],'won_pile':[]}
+    return player
 
 
 
 def init_game()->dict:
-    pass
+    players=[]
+
+    players.append(create_player())
+    players.append(create_player(input('Please enter your name.')))
+    deck1=deck.create_deck()
+    deck1=deck.shuffle(deck1)
+    for i in range(52):
+        if i < 26:
+            players[1]['hand'].append(deck1[i])
+        else:
+            players[0]['hand'].append(deck1[i])
+    return players
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import random
+from game_logic import game
 
-
-
-
+a=game.init_game()
+print(len(a[0]['hand']))
 
 
 
