195d58fe4533b97332f3ec4669d98b1b07fb5c5d
DavidPopovich3261 <8534397@gmail.com>
2025-10-23 09:55:36 +0300

    create_card1

--- utils/deck.py
+++ utils/deck.py
@@ -5,6 +5,7 @@
 def create_card(rank:str,suite:str) -> dict:
     value_cards={"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":11,"Q":12,"K":13}
     card={"rank":rank,"suite":suite,'value':value_cards[rank]}
+    return card
 
 
 def compare_cards(p1_card:dict, p2_card:dict) -> str:
