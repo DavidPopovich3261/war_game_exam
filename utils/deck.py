283f4e44da2b6feab36735edf31969c0647191c4
DavidPopovich3261 <8534397@gmail.com>
2025-10-23 10:07:28 +0300

    compare_cards1

--- utils/deck.py
+++ utils/deck.py
@@ -3,13 +3,20 @@
 
 
 def create_card(rank:str,suite:str) -> dict:
-    value_cards={"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":11,"Q":12,"K":13}
+    value_cards={"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":11,"Q":12,"K":13,"A":14}
     card={"rank":rank,"suite":suite,'value':value_cards[rank]}
     return card
 
 
+
 def compare_cards(p1_card:dict, p2_card:dict) -> str:
-    pass
+    if p1_card["value"] > p2_card["value"]:
+        result="p1"
+    elif p2_card['value'] > p1_card['value']:
+        result='p2'
+    else:
+        result="WAR"
+    return result
 
 
 def create_deck() -> list[dict]:
